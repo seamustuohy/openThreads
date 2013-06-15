ecffeed0f6c2839903f751d71234c154805207cf
Seamus Tuohy <s2e@opentechinstitute.org>
2013-06-15 17:45:21 -0400

    further cleaning

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='openThreads',
+      version='0.2',
+      description='An e-mail parser for exploring social communication',
+      long_description='OpenThreads parses e-mail list-servs to identify the way that individuals impact the comunicative environment',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: GPLV3',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: e-mail parsing',
+        ],
+      keywords='e-mail, list-serv',
+      url='https://github.com/elationfoundation/openThreads',
+      author='s2e',
+      author_email='s2e@opentechinstitute.org',
+      license='GPLV3',
+      packages=['OpenThreads'])
